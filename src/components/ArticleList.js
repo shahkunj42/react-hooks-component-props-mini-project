fc96241fd6093490204ec1fd97e01bff96bba23e
Kunj <shahkunj42@gmail.com>
2022-06-27 12:37:07 -0400

    commit tree 92655955514df299535c7ec47eda5821c35fc7b5

---
+++ src/components/ArticleList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Article from './Article.js'
+
+function ArticleList({posts}){
+    let postsList = posts.map((post) => (
+    <Article
+        key ={post.id}
+        title ={post.title}
+        date ={post.date}
+        preview ={post.preview}
+    />
+    ))
+    
+    return(
+        <main>
+            {postsList}
+        </main>
+    )
+}
+
+export default ArticleList;
